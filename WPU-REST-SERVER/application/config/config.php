26529e9bcf7021f781fc012f4048fbb737f21652
Hasnatul Fadillah <uuhasna906@gamil.com>
2025-05-29 19:30:58 +0700

    membuat rest client dan menambahkan profil

--- WPU-REST-SERVER/application/config/config.php
+++ WPU-REST-SERVER/application/config/config.php
@@ -23,12 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/rest-API/WPU-REST-SERVER';
+$config['base_url'] = 'http://localhost/rest-API/WPU-REST-SERVER/';
 
-
-
-// video 7 ,menambahkan dat ke database menggunakan postman
-// http://localhost/rest-API/WPU-REST-SERVER/api/mahasiswa
 /*
 |--------------------------------------------------------------------------
 | Index File
